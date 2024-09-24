7c342a97db5df82555a389d9d0d2ef0bf91dc0b4
NicolasChambon <nico.chambon@gmail.com>
2024-09-24 23:37:31 +0200

    starting fixture creation : users and sports

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,65 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use DateTime;
+use App\Entity\User;
+use App\Entity\Sport;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher
+    ){
+
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadUsers($manager);
+        $this->loadSports($manager);
+    }
+
+    public function loadUsers(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setPseudo('user' . $i)
+                ->setPassword($this->hasher->hashPassword($user, 'user' . $i))
+                ->setEmail('user' . $i . '@doe.fr')
+                ->setFirstname('John')
+                ->setLastname('Doe')
+                ->setBirthdate(new \DateTimeImmutable('1980-01-01'))
+                ->setCity('Cadolive')
+                ->setCreatedAt(new \DateTimeImmutable())
+                ->setThumbnail('https://loremflickr.com/320/240/portrait')
+                ->setIsVerified(true);
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+    public function loadSports(ObjectManager $manager): void
+    {
+        $sport1 = $this->createSport($manager, 'Escalade');
+        $sport2 = $this->createSport($manager, 'Randonnée');
+        $sport3 = $this->createSport($manager, 'Ski de randonnée');
+        $sport4 = $this->createSport($manager, 'VTT');
+        $sport5 = $this->createSport($manager, 'Surf');
 
         $manager->flush();
     }
+
+    public function createSport(ObjectManager $manager, string $name): Sport
+    {
+        $sport = new Sport();
+        $sport->setName($name)
+            ->setCreatedAt(new \DateTimeImmutable());
+        $manager->persist($sport);
+        return $sport;
+    }
+
 }
